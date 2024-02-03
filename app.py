5375f890c82539b358c428c46b39de0b6e3d1d1d
noway <dragonfury1928@gmail.com>
2024-02-03 11:09:55 -0500

    Chapters 1-3 Complete
    Learning strings, math, and importing

---
+++ app.py
@@ -0,0 +1,201 @@
+# External Imports
+from math import *
+
+
+# ====================
+# Chapter 1: Getting Started
+# ====================
+# print("hello world")
+#
+# print("hello world")
+#
+# character_name = "John"
+#
+# character_age = 34
+#
+# print("Hello " + character_name + "!" + " How are you?" + " I hope you are doing well." + " I am doing well too." + " I am just learning Python."
+# + "I am " + str(character_age) + " years old.")
+#
+# character_name = "Tom"
+#
+# print("Hello " + character_name + "!" + " How are you?" + " I hope you are doing well." + " I am doing well too." + " I am just learning Python."
+# + "I am " + str(character_age) + " years old.")
+#
+# character_age = 50
+#
+# print("Hello " + character_name + "!" + " How are you?" + " I hope you are doing well." + " I am doing well too." + " I am just learning Python."
+# + "I am " + str(character_age) + " years old.")
+#
+# character_male = True
+#
+# print("Hello " + character_name + "!" + " How are you?" + " I hope you are doing well." + " I am doing well too." + " I am just learning Python."
+# + "I am " + str(character_age) + " years old." + " I hope you are doing well." + str(character_male))
+
+# ====================
+# Chapter 2: Working with Strings
+# ====================
+# print("Giraffe\nAcademy")  # This is how to print a new line
+# print("Giraffe\"Academy")  # This is how to print a double quote
+#
+# phrase = "Giraffe Academy"
+# print(phrase + " is cool")  # This is how to concatenate strings
+# print(phrase.lower())  # This is how to convert a string to lower case
+# print(phrase.upper())  # This is how to convert a string to upper case
+# print(phrase.isupper())  # This is how to check if a string is upper case
+# print(phrase.upper().isupper())  # This is how to check if a string is upper case
+# print(len(phrase))  # This is how to get the length of a string
+# print(phrase[0])  # This is how to get the first character of a string (0 based index)
+# print(phrase.index("G"))  # This is how to get the index of first occurrence of a character
+# print(phrase.index("Acad"))  # This is how to get the index of first occurrence of a sequence of characters
+# print(phrase.replace("Giraffe", "Elephant"))  # This is how to replace a string with another string
+# print(phrase.replace("Giraffe", "Elephant").replace("Academy", "School")
+#       .replace("Elephant", "Something"))  # This is how to replace a string with another string multiple times
+# ====================
+# Chapter 3: Working with Numbers
+# ====================
+print("Chapter 3: Working with Numbers")
+print(2)  # This is how to print a number
+print(2.0987)  # This is how to print a decimal number
+print(-2.0987)  # This is how to print a negative number
+print(3 + 4.5)  # This is how to add two numbers
+print(str(3 * 4 + 5) + " This is my favorite")  # This is how to convert a number to a string
+# print(3 * 4 + " This is my favorite")  # This does not work because you cannot concatenate a number with a string
+
+print(10 % 3)  # This is how to get the remainder of a division
+my_num = 5
+
+print(pow(3, 2))  # This is how to raise a number to a power
+print(max(4, 6))  # This is how to get the maximum of two numbers
+print(min(4, 6))  # This is how to get the minimum of two numbers
+print(round(3.50))  # Rounds up from 3.5 to 4
+print(round(3.49))  # Rounds down from 3.49 to 3
+
+# Using the math library
+print(floor(3.7))  # Always rounds down
+print(ceil(3.7))  # Always rounds up
+print(sqrt(36))  # This is how to get the square root of a number
+# ====================
+# Chapter 4: Getting Input from Users
+# ====================
+
+# print("Chapter 4: Getting Input from Users")
+
+# ====================
+# Chapter 5: Building a Basic Calculator
+# ====================
+
+# print("Chapter 5: Building a Basic Calculator")
+
+# ====================
+# Chapter 6: Mad Libs Game
+# ====================
+
+# print("Chapter 6: Mad Libs Game")
+
+# ====================
+# Chapter 7: Lists
+# ====================
+
+# print("Chapter 7: Lists")
+
+# ====================
+# Chapter 8: List Functions
+# ====================
+
+# print("Chapter 8: List Functions")
+
+# ====================
+# Chapter 9: Tuples
+# ====================
+
+# print("Chapter 9: Tuples")
+
+# ====================
+# Chapter 10: Functions
+# ====================
+
+# print("Chapter 10: Functions")
+
+# ====================
+# Chapter 11: Return Statement
+# ====================
+
+# ====================
+# Chapter 12: If Statements
+# ====================
+
+# ====================
+# Chapter 13: If Statements & Comparisons
+# ====================
+
+# ====================
+# Chapter 14: Building a better Calculator
+# ====================
+
+# ====================
+# Chapter 15: Dictionaries
+# ====================
+
+# ====================
+# Chapter 16: While Loop
+# ====================
+
+# ====================
+# Chapter 17: Building a Guessing Game
+# ====================
+
+# ====================
+# Chapter 18: For Loop
+# ====================
+
+# ====================
+# Chapter 19: Exponent Function
+# ====================
+
+# ====================
+# Chapter 20: 2D Lists & Nested Loops
+# ====================
+
+# ====================
+# Chapter 21: Building a Translator
+# ====================
+
+# ====================
+# Chapter 22: Comments
+# ====================
+
+# ====================
+# Chapter 23: Try/Except
+# ====================
+
+# ====================
+# Chapter 24: Reading Files
+# ====================
+
+# ====================
+# Chapter 25: Writing to Files
+# ====================
+
+# ====================
+# Chapter 26: Modules & Pip
+# ====================
+
+# ====================
+# Chapter 27: Classes & Objects
+# ====================
+
+# ====================
+# Chapter 28: Building a Multiple Choice Quiz
+# ====================
+
+# ====================
+# Chapter 29: Object Functions
+# ====================
+
+# ====================
+# Chapter 30: Inheritance
+# ====================
+
+# ====================
+# Chapter 31: Python Interpreter
+# ====================
